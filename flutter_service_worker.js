315973b8e586b0a9e524694a8d9fc8b8d56c7ebf
sanghyun8039 <chltkdgus10@naver.com>
2024-01-11 15:04:53 +0900

    remove flexible

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -133,9 +133,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c99136216ca2e1938144f1891c783519
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "78f775f6dd8dd2309ec2f53f9290fba1",
-"/": "78f775f6dd8dd2309ec2f53f9290fba1",
-"main.dart.js": "172f9115b1e35ecf3eb0791222f27a6d",
+"index.html": "fc32d8b4c33f08db56ebf9a5c0fc2e04",
+"/": "fc32d8b4c33f08db56ebf9a5c0fc2e04",
+"main.dart.js": "3a9f229929d48cd75e1dcb65b1118b9d",
 "manifest.json": "71e0154c18320c9ca4cbf26c3e30935d",
 "version.json": "322dfa94430cc69aa45ce83495f46466"};
 // The application shell files that are downloaded before a service worker can
