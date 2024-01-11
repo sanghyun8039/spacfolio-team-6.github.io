dbaf927e49458bd011fd0254b9a7205e84199260
sanghyun8039 <chltkdgus10@naver.com>
2024-01-11 15:26:19 +0900

    remove safearea / expand/ flex/ spacer

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -133,9 +133,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c99136216ca2e1938144f1891c783519
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7c3aeff7a06d27ae22a046195b64ad39",
-"/": "7c3aeff7a06d27ae22a046195b64ad39",
-"main.dart.js": "bf31ae463661a6013f772b5532bd9796",
+"index.html": "47667d3e384a9f63c053228546854ba2",
+"/": "47667d3e384a9f63c053228546854ba2",
+"main.dart.js": "0049e7fe50b6db1096f1926c96dba4c0",
 "manifest.json": "71e0154c18320c9ca4cbf26c3e30935d",
 "version.json": "322dfa94430cc69aa45ce83495f46466"};
 // The application shell files that are downloaded before a service worker can
